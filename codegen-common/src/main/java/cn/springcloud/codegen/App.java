79e7ab85ccb11d38da7836fad3b9121f4cd8648a
xujin <Software_King@qq.com>
2018-01-18 00:36:07 +0800

    init

---
+++ codegen-common/src/main/java/cn/springcloud/codegen/App.java
@@ -0,0 +1,13 @@
+package cn.springcloud.codegen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
