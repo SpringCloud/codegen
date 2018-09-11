9c8df02fe00366987b89b63bc09290723b94e4d4
liyunlong <liyunlong@hujinloans.com>
2018-09-11 12:45:17 +0800

    更改为get方式

--- codegen-web/src/main/java/cn/springcloud/codegen/controller/CodegenController.java
+++ codegen-web/src/main/java/cn/springcloud/codegen/controller/CodegenController.java
@@ -4,16 +4,18 @@ import cn.springcloud.codegen.entity.Metadata;
 import cn.springcloud.codegen.entity.ProjectModel;
 import cn.springcloud.codegen.service.ComponentExecutor;
 import cn.springcloud.codegen.util.CodeGenTransport;
+import cn.springcloud.codegen.util.JsonMapper;
 import freemarker.template.TemplateException;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -30,14 +32,15 @@ public class CodegenController {
     @Autowired
     private ComponentExecutor componentExecutor;
 
-    @RequestMapping(value = "/download", method = RequestMethod.POST, consumes = "application/json",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<Resource> downloadResponse(@RequestBody ProjectModel projectModel ) throws IOException, TemplateException, URISyntaxException {
-        return new CodeGenTransport(componentExecutor).downloadResponse(projectModel);
-    }
+//    @RequestMapping(value = "/download", method = RequestMethod.POST, consumes = "application/json",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+//    public ResponseEntity<InputStreamResource> downloadResponse(@RequestBody ProjectModel projectModel ) throws IOException, TemplateException, URISyntaxException {
+//        return new CodeGenTransport(componentExecutor).downloadResponse(projectModel, response);
+//    }
 
-    @RequestMapping(value = "/download/bytes", method = RequestMethod.POST, consumes = "application/json")
-    public byte[] downloadBytes(@RequestBody ProjectModel projectModel) throws IOException, TemplateException {
-        return new CodeGenTransport(componentExecutor).downloadBytes(projectModel);
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void downloadResponse(@RequestParam String json, HttpServletResponse response ) throws IOException, TemplateException, URISyntaxException {
+        ProjectModel projectModel = JsonMapper.nonDefaultMapper().fromJson(json, ProjectModel.class);
+        new CodeGenTransport(componentExecutor).downloadResponse(projectModel, response);
     }
 
 }
--- codegen-web/src/main/java/cn/springcloud/codegen/util/CodeGenTransport.java
+++ codegen-web/src/main/java/cn/springcloud/codegen/util/CodeGenTransport.java
@@ -4,21 +4,14 @@ import cn.springcloud.codegen.engine.entity.InputParams;
 import cn.springcloud.codegen.engine.tools.ClassTools;
 import cn.springcloud.codegen.entity.ProjectModel;
 import cn.springcloud.codegen.service.ComponentExecutor;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 /**
  * @author Vincent.
@@ -38,35 +31,18 @@ public class CodeGenTransport {
         return componentExecutor.generate(inputParams);
     }
 
-    public ResponseEntity<Resource> downloadResponse(ProjectModel projectModel) throws URISyntaxException, IOException {
-        String canonicalFileName = getCanonicalFileName(projectModel.getProjectName());
-
+    public void downloadResponse(ProjectModel projectModel, HttpServletResponse response) throws URISyntaxException, IOException {
         InputParams inputParams = new InputParams();
         inputParams.setParamMap(ClassTools.buildFieldValueToMap(projectModel));
         byte[] bytes = componentExecutor.generate(inputParams);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
-        headers.add("charset", "utf-8");
-
-        headers.add("Content-Disposition", "attachment;filename=\"" + canonicalFileName + "\"");
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .header("Content-Disposition", "attachment; filename=" + canonicalFileName + ".zip")
-                .body(new InputStreamResource(new ByteArrayInputStream(bytes)));
+        response.setContentType("application/x-download");
+        response.addHeader("Content-Disposition", "attachment;filename=codegen.zip");
+        OutputStream outputStream = response.getOutputStream();
+        InputStream inputStream = new ByteArrayInputStream(bytes);
+        IOUtils.copy(inputStream, outputStream);
+        outputStream.flush();
+        response.flushBuffer();
 
     }
 
-    private String getCanonicalFileName(String fileName) {
-        if (StringUtils.isEmpty(fileName)) {
-            throw new IllegalArgumentException("File name is null or empty");
-        }
-        try {
-            return URLEncoder.encode(fileName + ".zip", "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalArgumentException(e.getMessage(), e);
-        }
-    }
 }
